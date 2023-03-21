bf4becf7dd3f300d4ae4dabe866bb943d4ae1861
ar7casper <alex@thesashka.com>
2023-03-21 15:43:31 +0200

    Initial commit

---
+++ apps/foo/src/app/app.tsx
@@ -0,0 +1,26 @@
+
+import styled from '@emotion/styled';
+
+import NxWelcome from "./nx-welcome";
+
+
+const StyledApp = styled.div`
+    // Your style here
+`;
+
+
+export function App() {
+
+  return (
+      <StyledApp>
+        
+        <NxWelcome title="foo"/>
+        
+      </StyledApp>
+  );
+
+}
+
+
+export default App;
+
